d9303644e43c990b2f2e920d023eb680bc7158ed
Neha Prabhu <nehaprabhup17@gmail.com>
2025-06-24 16:00:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,10 @@ app.get("/api/:date", function(req, res) {
   });
 });
 
+app.get("/api", (req, res) => {
+  const date = new Date();
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
